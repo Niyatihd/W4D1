bbdc692ab356f138401d9e7c471469e86548ec01
Niyati Desai <niyatihd@gmail.com>
2019-01-29 11:41:04 -0800

    Add w4d1 rails project

---
+++ first_routes/app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+  
+  def index
+    users = User.all
+
+    render json: users
+
+  end
+
+  def create
+    user = User.new(params.require(:user).permit(:name, :email))
+    
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  def update
+    user = User.find(params[:id])
+
+    if user.update(user_params)
+      # user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+
+  end
+
+  def user_params
+    params.require(:user).permit(:email)
+  end
+  
+
+  
+end
